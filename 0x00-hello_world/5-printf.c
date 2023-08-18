9e91970cee275e1647451c176e059c84dca14b98
Emmanuel-Sasere <emmanuelscript.code@gmail.com>
2023-08-18 03:19:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - This is a program that print a line using the printf functions
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
