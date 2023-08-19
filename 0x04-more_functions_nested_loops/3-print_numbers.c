a1d442248c240f257a593f9f079ae4fcc416211c
Emmanuel-Sasere <emmanuelscript.code@gmail.com>
2023-08-19 05:16:39 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - This function prints the numbers, from 0 to 9
+ *
+ * Return: Always 0.
+ */
+void  print_numbers(void)
+{
+int x;
+for (x = 48; x < 58; x++)
+{
+int_putchar(x);
+}
+_putchar('\n');
+}
